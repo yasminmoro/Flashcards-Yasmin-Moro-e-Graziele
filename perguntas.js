1eb00d6354652aaf9a08bfb5a4cd949f4892bbee
yasmin moro <125415435+yasminmoro@users.noreply.github.com>
2025-08-08 12:08:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação', //Tema do cartão;  
+    'O que é Python?', // Adicionar nova pergunta; 
+    'O Python é uma linguagem de programação'  // Adicionar uma nova resposta à pergunta anterior;
+)
+
+criaCartao(
+    'Geografia', //Tema do cartão;  
+    'Qual a capital da França?', // Adicionar nova pergunta; 
+    'A capital da França é Paris' // Adicionar uma nova resposta à pergunta anterior;
+)
+
+criaCartao(
+    'Programação', //Tema do cartão;  
+    'O que é uma função?', // Adicionar nova pergunta; 
+    'Uma função é um bloco de código que executa alguma tarefa' // Adicionar uma nova resposta à pergunta anterior;
+)
+
+criaCartao(
+    'Lingua inglesa', //Tema do cartão;  
+    'Como se diz oi em Inglês?', // Adicionar nova pergunta;
+    'Oi em ingles é HI (RAI)' // Adicionar uma nova resposta à pergunta anterior;
+)
+
+//Observações:
+// Para criar mais cartões, basta copiar e colar algumas dessas funções "criaCartão" 
+// Altere as informações dos cartões (tema, frente e verso do cartão);
+// Use sua criatividade, faça das disciplinas que você está estudando agora. Ex: Português, Matemática, Inglês, História, Geografia, Filosofia, Sociologia, Projeto de Vida, ect.
